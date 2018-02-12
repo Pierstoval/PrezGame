2e9478b17a422d20d88ea00b3ba528989095c6e8
Alex Rock Ancelet <pierstoval@gmail.com>
2018-02-12 20:40:37 +0100

    Remove useless code

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,11 +6,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class DefaultController extends AbstractController
 {
